920df8db19dbcca639ba9823f69ecdface01ef25
Thong Le <thongle.aisolution@gmail.com>
2020-10-21 13:06:16 +0700

    sanpham v1

--- details/models.py
+++ details/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class SanPham(models.Model):
-	sku = models
+	sku = models.CharField(max_length=100,default='some_value')
 	title = models.CharField(max_length=100)
 	price = models.IntegerField()
 	stock = models.BooleanField(max_length=25)
-	image = models.FileField(upload_to='uploads/%Y/%m/%d/')
+	image = models.FileField(upload_to='uploads/%Y/%m/%d/',default='some_value')
 	brief = models.CharField(max_length=100)
 	description = models.CharField(max_length=1000)
