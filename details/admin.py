2f40ada00b87b03392e90f29e101eb3457748bf6
Thong Le <thongle.aisolution@gmail.com>
2020-10-22 17:38:43 +0700

    add danh muc

--- details/admin.py
+++ details/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(SanPham)
+class SanPhamAdmin(admin.ModelAdmin):
+    list_display = ("id", "sku", "price", "stock")
+
+
+admin.site.register(SanPham,SanPhamAdmin)
+admin.site.register(DanhMuc)
--- details/models.py
+++ details/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class DanhMuc(models.Model):
+	category = models.CharField(max_length = 20)
+
+	def __str__(self):
+		return f"{self.category}"
+
 class SanPham(models.Model):
 	sku = models.CharField(max_length=100,default='some_value')
 	title = models.CharField(max_length=100)
