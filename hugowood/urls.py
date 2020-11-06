245b3103698b3758c7f5de6c207d81e60e9a09ff
Thong Le <thongle.aisolution@gmail.com>
2020-11-06 13:10:27 +0700

    add user function

--- hugowood/urls.py
+++ hugowood/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('daika/', admin.site.urls),
     path('',include('website.urls')),
     path('details/',include('details.urls')),
+    path('users/',include('users.urls')),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
